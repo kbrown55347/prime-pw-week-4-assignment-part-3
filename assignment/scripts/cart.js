aa79ca30edebc582ddb8dd25d28953720df8a476
Kelsey Brown <kbrown55347@gmail.com>
2021-10-12 12:33:15 -0700

    created addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,17 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = []
+
+console.log(basket);
+
+function addItem(item) {
+  basket.push(item);
+  return true;
+}
+
+console.log('Item added?', addItem('broccoli'));
+console.log('Item added?', addItem('apples'));
+console.log('Item added?', addItem('cookies'));
+console.log(basket);
