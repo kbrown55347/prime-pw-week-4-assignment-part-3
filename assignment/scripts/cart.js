0dd05dc77514e56e455dc8afe7e61978fb82c877
Kelsey Brown <kbrown55347@gmail.com>
2021-10-13 09:47:57 -0700

    Added question in index.html

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,7 +4,7 @@ console.log('***** Cart Functions *****');
 
 let basket = []
 
-console.log(basket);
+console.log('Testing empty array:', basket);
 
 function addItem(item) {
   basket.push(item);
@@ -14,11 +14,11 @@ function addItem(item) {
 console.log('Item added?', addItem('broccoli'));
 console.log('Item added?', addItem('apples'));
 console.log('Item added?', addItem('cookies'));
-console.log(basket);
+console.log('What is now in my basket?', basket);
 
 function listItems() {
   for (i=0; i<basket.length; i++) {
-    console.log(basket[i]);
+    console.log('Item in my basket:', basket[i]);
   }
 }
 
